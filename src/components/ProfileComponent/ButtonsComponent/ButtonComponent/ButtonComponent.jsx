df6133e6769ec820f69270535600230d5fe9d2e3
jfiora <sillonpeludo@gmail.com>
2021-11-04 15:06:43 -0300

    button component

---
+++ src/components/ProfileComponent/ButtonsComponent/ButtonComponent/ButtonComponent.jsx
@@ -0,0 +1,16 @@
+import './ButtonComponent.css';
+
+const ButtonComponent = ({ time, isSelected }) => {
+
+    return(
+        <button
+            className="ButtonComponent"
+            onClick={() => setCurrentTime(time)}
+            style={{ color: isSelected ? '#ffffff' : '#5847eb'}}
+        >
+            <span>{time}</span>
+        </button>
+    );
+}
+
+export default ButtonComponent;
